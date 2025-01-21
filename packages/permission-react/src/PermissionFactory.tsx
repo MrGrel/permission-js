d98ffa90d3bfe2dca76a9c68d1d6a6541eed0238
Egor Medvedev <mr.grel1998@gamil.com>
2025-01-21 20:18:53 +0300

    update permission-react

--- packages/permission-react/src/PermissionFactory.tsx
+++ packages/permission-react/src/PermissionFactory.tsx
@@ -2,130 +2,79 @@ import {
   createContext,
   useCallback,
   useContext,
-  useMemo,
   useState,
 } from "react";
-import { PermissionBuilder } from "@permission-js/core";
+
 import {
+  PermissionBuilder,
   BaseActions,
   BaseConditions,
-  SubscribedCheckPermissions,
   CheckPermissions,
-  Subsets,
-  NoneOrderTuple,
-} from "./lib/types";
-import { typedMemo } from "utils/typedMemo";
-import { AbilityBuilder, createMongoAbility } from "@casl/ability";
+} from "permission-js-core";
 
-interface IContextProps<
-  S extends string,
-  A extends BaseActions<S>,
-  C extends BaseConditions<S>
-> {
-  subscribedCheck: PermissionBuilder<S, A, C>["subscribedCheck"];
-}
+import { typedMemo } from "./utils/typedMemo";
 
 export const reactFactoryPermission = <
   S extends string,
   A extends BaseActions<S>,
   C extends BaseConditions<S>
 >() => {
-  const Context = createContext<IContextProps<S, A, C> | null>(null);
-
-  const { checkPermission, update, subscribedCheck } = new PermissionBuilder<
-    S,
-    A,
-    C
-  >();
+  const Context = createContext<PermissionBuilder<S, A, C> | null>(null);
 
   const PermissionProvider = ({ children }: { children: React.ReactNode }) => {
-    const contextValue = useMemo(() => subscribedCheck, []);
+    const [contextValue] = useState(() => new PermissionBuilder<S, A, C>());
 
-    return (
-      <Context.Provider value={{ subscribedCheck: contextValue }}>
-        {children}
-      </Context.Provider>
-    );
+    return <Context.Provider value={contextValue}>{children}</Context.Provider>;
   };
 
   const usePermission = () => {
     const context = useContext(Context);
-    if (!context) throw new Error("Use subscribing check without provider");
+    if (!context)
+      throw new Error("Use subscribing check without PermissionProvider");
 
-    const { subscribedCheck } = context;
+    const { subscribedCheck, update } = context;
     const [signal, setSignal] = useState(false);
 
     const trigger = useCallback(() => {
       setSignal((prev) => !prev);
     }, [signal]);
 
-    const can = <
-      Subjects extends S,
-      Action extends NoneOrderTuple<A[Subjects]>
-    >(
-      args: CheckPermissions<Subjects, C, Action>
-    ) => {
-      return subscribedCheck({ ...args, signal: trigger });
-    };
+    const can = useCallback(
+      <Subjects extends S, Action extends A[Subjects]>(
+        args: CheckPermissions<Subjects, Action, C>
+      ) => {
+        return subscribedCheck({ ...args, signal: trigger });
+      },
+      [setSignal]
+    );
 
-    return { can };
+    return { can, update };
   };
 
-  const Can = <Subjects extends S, Action extends Subsets<A[Subjects]>>({
-    children,
-    element = null,
-    ...args
-  }: CheckPermissions<Subjects, C, Action> & {
-    children: React.ReactNode;
-    element?: React.ReactNode;
-  }): React.ReactNode => {
-    const { can } = usePermission();
+  const Can = typedMemo(
+    <Subjects extends S, Action extends A[Subjects]>({
+      children,
+      element = null,
+      ...args
+    }: CheckPermissions<Subjects, Action, C> & {
+      children: React.ReactNode;
+      element?: React.ReactNode;
+    }): React.ReactNode => {
+      const { can } = usePermission();
 
-    const result = can(args);
+      const result = can(args);
 
-    if (result) {
-      return children;
+      if (result) {
+        return children;
+      }
+
+      return element;
     }
-
-    return element;
-  };
+  );
 
   return {
     usePermission,
-    checkPermission,
-    updatePermission: update,
     Can,
     PermissionProvider,
   };
-};
-
-type keys = "image" | "article" | "comment" | "like";
-interface Actions {
-  image: ["upload", "download"];
-  article: ["create", "update", "delete", "upload", "download"];
-  comment: ["create", "update", "delete"];
-  like: ["create", "delete"];
-}
-
-interface TBaseConditions {
-  image: never;
-  article: never;
-  comment: never;
-  like: never;
-}
-
-const { Can, usePermission } = reactFactoryPermission<
-  keys,
-  Actions,
-  TBaseConditions
->();
-
-const Art = () => (
-  <Can subject={"article"} action={["", "", '']}>
-    asdf
-  </Can>
-);
-const useSuka = () => {
-  const { can } = usePermission();
-  can({ subject: "image", action: ['download', 'download']});
 };
--- packages/permission-react/vite.config.ts
+++ packages/permission-react/vite.config.ts
@@ -1,15 +1,27 @@
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+
 import { defineConfig } from "vite";
 import path from "path";
 
 export default defineConfig({
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/index.tsx"),
       name: "PermissionReact",
-      fileName: (format) => `permission-react.${format}.js`,
+      fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
       external: ["react", "@permission/core"],
     },
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
   },
 });
